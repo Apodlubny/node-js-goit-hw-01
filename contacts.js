99b251cacf88a6baad51ef10b4d3b7c748470329
Apodlubny <apodlubny@gmail.com>
2022-12-10 16:14:16 +0200

    final

--- contacts.js
+++ contacts.js
@@ -9,9 +9,34 @@ const listContacts = async () => {
   return contacts;
 };
 
-const getContactsById = async () => {
+const getContactById = async (contactId) => {
   const contacts = await listContacts();
   const result = contacts.find((item) => item.id === `${contactId}`);
   return result || null;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const lastElement = contacts[contacts.length - 1];
+  const id = Number(lastElement.id) + 1;
+
+  const newContact = { id: `${id}`, name, email, phone };
+
+  contacts.push(newContact);
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return newContact;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === `${contactId}`);
+
+  if (index === -1) return null;
+
+  const [removeContact] = contacts.splice(index, 1);
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return removeContact;
 };
 module.exports = { listContacts, getContactById, addContact, removeContact };
